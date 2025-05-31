8fd560e5e3d415b1d1c263b41bc50e208fa19c7b
mohammad rida <mhmd_rida_990@hotmail.com>
2025-05-31 19:17:23 +0300

    add banner to redirect to client

--- src/app/(dashboard)/[storeId]/layout.tsx
+++ src/app/(dashboard)/[storeId]/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/myComponents/Navbar";
 import { prisma } from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs/server";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 const DashboardLayout = async ({
@@ -28,6 +29,19 @@ const DashboardLayout = async ({
   }
   return (
     <>
+      <div className="flex items-center justify-center bg-sky-200 w-full space-x-4 h-10">
+        <span className="text-sm md:text-lg text-gray-500">
+          View Store in E-Commerce Client:
+        </span>
+
+        <Link
+          href="https://ecommerce-store-rho-three.vercel.app/"
+          target="_blink"
+          className="underline text-green-700 hover:text-green-400 text-sm md:text-lg"
+        >
+          E-Commerce Client
+        </Link>
+      </div>
       <Navbar />
       <div>{children}</div>;
     </>
